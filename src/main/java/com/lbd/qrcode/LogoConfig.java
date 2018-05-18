e017d21e6c7013cd2272caaebf1fb771d6ed57d7
zhida.wen <zhida_wen@163.com>
2018-05-18 21:59:06 +0800

    modify some

--- src/main/java/com/lbd/qrcode/LogoConfig.java
+++ src/main/java/com/lbd/qrcode/LogoConfig.java
@@ -8,12 +8,18 @@ import java.awt.*;
  * @Description
  */
 public class LogoConfig {
-    // logo默认边框颜色
-    public static final Color DEFAULT_BORDERCOLOR = Color.WHITE;
-    // logo默认边框宽度
+    /**
+     * logo默认边框颜色
+     */
+    public static final Color DEFAULT_BORDER_COLOR = Color.WHITE;
+    /**
+     * logo默认边框宽度
+     */
     public static final int DEFAULT_BORDER = 2;
-    // logo大小默认为照片的1/6
-    public static final int DEFAULT_LOGOPART = 6;
+    /**
+     * logo大小默认为照片的1/6
+     */
+    public static final int DEFAULT_LOGO_PART = 6;
 
     private final int border = DEFAULT_BORDER;
     private final Color borderColor;
@@ -23,29 +29,24 @@ public class LogoConfig {
      * Creates a default config with on color {@link #BLACK} and off color
      * {@link #WHITE}, generating normal black-on-white barcodes.
      */
-    public LogoConfig()
-    {
-        this(DEFAULT_BORDERCOLOR, DEFAULT_LOGOPART);
+    public LogoConfig() {
+        this(DEFAULT_BORDER_COLOR, DEFAULT_LOGO_PART);
     }
 
-    public LogoConfig(Color borderColor, int logoPart)
-    {
+    public LogoConfig(Color borderColor, int logoPart) {
         this.borderColor = borderColor;
         this.logoPart = logoPart;
     }
 
-    public Color getBorderColor()
-    {
+    public Color getBorderColor() {
         return borderColor;
     }
 
-    public int getBorder()
-    {
+    public int getBorder() {
         return border;
     }
 
-    public int getLogoPart()
-    {
+    public int getLogoPart() {
         return logoPart;
     }
 }
--- src/main/java/com/lbd/qrcode/MatrixToImageWriter.java
+++ src/main/java/com/lbd/qrcode/MatrixToImageWriter.java
@@ -5,7 +5,6 @@ import com.google.zxing.EncodeHintType;
 import com.google.zxing.MultiFormatWriter;
 import com.google.zxing.common.BitMatrix;
 import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
-import com.google.zxing.qrcode.encoder.QRCode;
 import com.sun.image.codec.jpeg.JPEGCodec;
 import com.sun.image.codec.jpeg.JPEGImageEncoder;
 
@@ -69,12 +68,9 @@ public class MatrixToImageWriter {
      * @param qrPic
      * @param logoPic
      */
-    public static void addLogo_QRCode(File qrPic, File logoPic, LogoConfig logoConfig)
-    {
-        try
-        {
-            if (!qrPic.isFile() || !logoPic.isFile())
-            {
+    public static void addLogoToQRCode(File qrPic, File logoPic, LogoConfig logoConfig) {
+        try {
+            if (!qrPic.isFile() || !logoPic.isFile()) {
                 System.out.print("file not find !");
                 System.exit(0);
             }
@@ -90,14 +86,13 @@ public class MatrixToImageWriter {
              */
             BufferedImage logo = ImageIO.read(logoPic);
 
-            int widthLogo = image.getWidth()/logoConfig.getLogoPart();
-            //    int    heightLogo = image.getHeight()/logoConfig.getLogoPart();
-            int    heightLogo = image.getWidth()/logoConfig.getLogoPart(); //保持二维码是正方形的
+            int widthLogo = image.getWidth() / logoConfig.getLogoPart();
+            //保持二维码是正方形的
+            int heightLogo = image.getWidth() / logoConfig.getLogoPart();
 
             // 计算图片放置位置
             int x = (image.getWidth() - widthLogo) / 2;
-            int y = (image.getHeight() - heightLogo) / 2 ;
-
+            int y = (image.getHeight() - heightLogo) / 2;
 
             //开始绘制图片
             g.drawImage(logo, x, y, widthLogo, heightLogo, null);
@@ -109,15 +104,14 @@ public class MatrixToImageWriter {
             g.dispose();
 
             ImageIO.write(image, "jpeg", new File("D:/newPic.jpg"));
-        }
-        catch (Exception e)
-        {
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
     /**
-     * @为图片添加文字
+     * 为图片添加文字
+     *
      * @param pressText 文字
      * @param newImg    带文字的图片
      * @param targetImg 需要添加文字的图片
@@ -125,15 +119,16 @@ public class MatrixToImageWriter {
      * @param color
      * @param fontSize
      * @param width
-     * @param heigh
+     * @param height
      */
-    public static void pressText(String pressText, String newImg, String targetImg, int fontStyle, Color color, int fontSize, int width, int height) {
+    public static void pressText(String pressText, String newImg, String targetImg,
+                                 int fontStyle, Color color, int fontSize, int width, int height) {
 
         //计算文字开始的位置
         //x开始的位置：（图片宽度-字体大小*字的个数）/2
-        int startX = (width-(fontSize*pressText.length()))/2;
+        int startX = (width - (fontSize * pressText.length())) / 2;
         //y开始的位置：图片高度-（图片高度-图片宽度）/2
-        int startY = height-(height-width)/2;
+        int startY = height - (height - width) / 2;
 
         try {
             File file = new File(targetImg);
@@ -176,7 +171,7 @@ public class MatrixToImageWriter {
             //设置UTF-8， 防止中文乱码
             hints.put(EncodeHintType.CHARACTER_SET, "UTF-8");
             //设置二维码四周白色区域的大小
-            hints.put(EncodeHintType.MARGIN,1);
+            hints.put(EncodeHintType.MARGIN, 1);
             //设置二维码的容错性
             hints.put(EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.H);
 
@@ -189,36 +184,36 @@ public class MatrixToImageWriter {
             BitMatrix bitMatrix = multiFormatWriter.encode(content, BarcodeFormat.QR_CODE, width, height, hints);
 
             //qrcFile用来存放生成的二维码图片（无logo，无文字）
-            File qrcFile = new File(path,"myPicture.jpg");
+            File qrcFile = new File(path, "myPicture.jpg");
             //logoFile用来存放带有logo的二维码图片（二维码+logo，无文字）
-            File logoFile = new File(path,"chopper.jpg");
+            File logoFile = new File(path, "chopper.jpg");
 
             //开始画二维码
             MatrixToImageWriter.writeToFile(bitMatrix, "jpg", qrcFile);
 
-            //在二维码中加入图片
-            LogoConfig logoConfig = new LogoConfig(); //LogoConfig中设置Logo的属性
-            addLogo_QRCode(qrcFile, logoFile, logoConfig);
+            //在二维码中加入图片 LogoConfig中设置Logo的属性
+            LogoConfig logoConfig = new LogoConfig();
+            addLogoToQRCode(qrcFile, logoFile, logoConfig);
 
-
-            int font = 18; //字体大小
-            int fontStyle = 1; //字体风格
+            //字体大小
+            int font = 18;
+            //字体风格
+            int fontStyle = 1;
 
             //用来存放带有logo+文字的二维码图片
-            String newImageWithText = "D:/imageWithText.jpg";
+            String newImageWithText = "E:/QRCodeImage/imageWithText.jpg";
             //带有logo的二维码图片
-            String targetImage = "D:/newPic.jpg";
+            String targetImage = "E:/QRCodeImage/newPic.jpg";
             //附加在图片上的文字信息
             String text = "文字测试一二三四五六123";
 
             //在二维码下方添加文字（文字居中）
-            pressText(text, newImageWithText, targetImage, fontStyle, Color.BLUE, font,  width,  height) ;
+            pressText(text, newImageWithText, targetImage, fontStyle, Color.BLUE, font, width, height);
 
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
-
 
 
 }
